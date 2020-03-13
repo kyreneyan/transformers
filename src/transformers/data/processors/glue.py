70476798b28afbe04901b401b262d302fc66a810
Yanyan_Xu <54363386+kyreneyan@users.noreply.github.com>
2020-03-13 10:04:41 -0400

    update1

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -517,13 +517,13 @@ class WnliProcessor(DataProcessor):
 
 
 class BoolqProcessor(DataProcessor):
-    """Processor for the WNLI data set (GLUE version)."""
+    """Processor for the BOOLQ data set (GLUE version)."""
 
     def get_example_from_tensor_dict(self, tensor_dict):
         """See base class."""
         return InputExample(
             tensor_dict["idx"].numpy(),
-            tensor_dict["sentence"].numpy().decode("utf-8"),
+            tensor_dict["sentence1"].numpy().decode("utf-8"),
             tensor_dict["sentence2"].numpy().decode("utf-8"),
             str(tensor_dict["label"].numpy()),
         )
